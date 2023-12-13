e2897fdf5c48fd31d1418ed966868bc90005d23f
Juano <graphographer@gmail.com>
2023-12-13 14:23:50 -0500

    add actions and computeds to app state

--- src/models/state.ts
+++ src/models/state.ts
@@ -1,19 +1,93 @@
 import { makeAutoObservable, observable } from 'mobx';
-import { TDataEntries } from './types';
+import { TDataEntries, TDataEntry } from './types';
 
 export class State {
-	selectedCountry: string = 'United States of America';
+	data: TDataEntries;
 
-	constructor(private data: TDataEntries) {
-		makeAutoObservable(this);
+	selectedCountry: string = '';
+	selectedEducationLevel: string = '';
+	selectedSkill: string = '';
+
+	constructor(data: TDataEntries) {
+		this.data = data;
+
+		makeAutoObservable(this, {
+			data: false
+		});
 	}
 
-	get countries() {
+	get allCountries() {
 		return [...new Set(this.data.map(entry => entry.Country).flat())];
+	}
+
+	get filteredCountries() {
+		const { selectedEducationLevel, selectedSkill } = this;
+
+		if (!selectedEducationLevel && !selectedSkill) {
+			return new Set();
+		}
+
+		const filteredCountries = new Set(
+			this.data
+				.filter(entry => {
+					const { 'Level of Education': educationLevels, Skills: skills } =
+						entry;
+
+					if (selectedEducationLevel && selectedSkill) {
+						return (
+							educationLevels.includes(selectedEducationLevel) &&
+							skills.includes(selectedSkill)
+						);
+					}
+
+					return (
+						educationLevels.includes(selectedEducationLevel) ||
+						skills.includes(selectedSkill)
+					);
+				})
+				.map(entry => entry.Country)
+				.flat()
+		);
+
+		return filteredCountries;
+	}
+
+	get projectsByCountry() {
+		const projectsByCountry = new Map<string, TDataEntries>(
+			this.allCountries.map(country => [country, []])
+		);
+
+		this.data.forEach(entry =>
+			entry.Country.forEach(country =>
+				projectsByCountry.get(country)?.push(entry)
+			)
+		);
+
+		return projectsByCountry;
+	}
+
+	get educationLevels() {
+		return [
+			...new Set(this.data.map(entry => entry['Level of Education']).flat())
+		];
+	}
+
+	get skillsMeasured() {
+		return [...new Set(this.data.map(entry => entry.Skills).flat())];
+	}
+
+	get challengeAreas() {
+		return [...new Set(this.data.map(entry => entry))];
 	}
 
 	setSelectedCountry(country: string) {
 		this.selectedCountry = country;
+	}
+	setEducationLevel(level: string) {
+		this.selectedEducationLevel = level;
+	}
+	setSkillsMeasured(skill: string) {
+		this.selectedSkill = skill;
 	}
 }
 
